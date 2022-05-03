60fd84ea915fe77be35e5e12bee57714bd9446bb
Tyler Duran <tymduran@gmail.com>
2022-05-02 17:01:40 -0700

    Updated

--- how_long_did_that_take.js
+++ how_long_did_that_take.js
@@ -1,48 +1,48 @@
-// Number.prototype.isPrime = function() {
-//     for( let i=2; i<=Math.sqrt(this); i++ ) {
-//         if( this % i === 0 ) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+Number.prototype.isPrime = function() {
+    for( let i=2; i<=Math.sqrt(this); i++ ) {
+        if( this % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
 
-// const { performance } = require('perf_hooks');
-// const start = performance.now();
-// let primeCount = 0;
-// let num = 2; // for math reasons, 1 is considered prime
-// while( primeCount < 1e4 ) {
-//     if( num.isPrime() ) {
-//         primeCount++;
-//     }
-//     num++;
-// }
-// console.log(`The 10,000th prime number is ${num-1}`);
-// console.log(`This took ${performance.now() - start} milliseconds to run`);
-
-
-// // recursive
-// function rFib( n ) {
-//     if ( n < 2 ) {
-//         return n;
-//     }
-//     return rFib( n-1 ) + rFib( n-2 );
-// }
-// console.log(rFib(20))
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while( primeCount < 1e4 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
 
 
-// // iterative
-// function iFib( n ) {
-//     const vals = [ 0, 1 ];
-//     while(vals.length-1 < n) {
-//         let len = vals.length;
-//         vals.push( vals[len-1] + vals[len-2] );
-//     }
-//     return vals[n];
-// }
-// console.log(rFib(20))
+// recursive
+function rFib( n ) {
+    if ( n < 2 ) {
+        return n;
+    }
+    return rFib( n-1 ) + rFib( n-2 );
+}
+console.log(rFib(20))
 
-// // Recursive is faster
+
+// iterative
+function iFib( n ) {
+    const vals = [ 0, 1 ];
+    while(vals.length-1 < n) {
+        let len = vals.length;
+        vals.push( vals[len-1] + vals[len-2] );
+    }
+    return vals[n];
+}
+console.log(rFib(20))
+
+// Recursive is faster
 
 
 
